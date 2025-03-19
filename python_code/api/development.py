e28fef181513986d48e8d01b0cb4c405272a1164
Shubham Kafle <shubhamkafle44@gmail.com>
2025-03-19 13:41:34 -0500

    Creating guardAgent

---
+++ python_code/api/development.py
@@ -0,0 +1,27 @@
+from agents import (GuardAgent)
+import os
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    guard_agent = GuardAgent()
+
+    messages = []
+    while True:
+        # Display the chat history
+        os.system('cls' if os.name == 'nt' else 'clear')
+        
+        print("\n\nPrint Messages ...............")
+        for message in messages:
+            print(f"{message['role'].capitalize()}: {message['content']}")
+
+        # Get user input
+        prompt = input("User: ")
+        messages.append({"role": "user", "content": prompt})
+
+        # Get GuardAgent's response
+        guard_agent_response = guard_agent.get_response(messages)
+        print(guard_agent_response)
+        messages.append(guard_agent_response)
